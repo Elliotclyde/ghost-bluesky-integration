1d0add51ec622c62da591b02207001006ba304f9
Hugh Haworth <hughelliotclyde@gmail.com>
2025-01-05 12:43:25 +1300

    Added Testing and refactored

---
+++ BlueskyClient.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+export class BlueskyClient {
+
+    constructor(blueskyIdentifier, blueskyPass) {
+        this.blueskyIdentifier = blueskyIdentifier
+        this.blueskyPass = blueskyPass
+    }
+
+    async createBlueskySession() {
+        const response = await axios.post('https://bsky.social/xrpc/com.atproto.server.createSession', {
+            identifier: this.blueskyIdentifier,
+            password: this.blueskyPass
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        return response
+    }
+
+    async createRecord(token, text, linkFacets) {
+        try {
+            const response = await axios.post('https://bsky.social/xrpc/com.atproto.repo.createRecord', {
+                collection: 'app.bsky.feed.post',
+                repo: this.blueskyIdentifier,
+                record: {
+                    text: text,
+                    createdAt: new Date().toISOString(),
+                    facets: linkFacets
+                }
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            console.log(response.data);
+        } catch (error) {
+            console.error(error.response ? error.response.data : error.message);
+        }
+    }
+}
--- test.js
+++ test.js
@@ -1,10 +1,40 @@
 import test from 'node:test';
 import('chai').then(runTests)
 import * as LightMyRequest from 'light-my-request'
+import { Server } from './server.js';
+
+const { inject } = LightMyRequest;
 
 function runTests(chai) {
-    test('synchronous passing test', (t) => {
+    test('Empty object should ', async (t) => {
+        const blueskyClient = {
+            createBlueskySession() { return 'asdf-asdf-asdf-asdf' },
+            createRecord() { return {} }
+        }
+        t.mock.method(blueskyClient, 'createBlueskySession')
+        t.mock.method(blueskyClient, 'createRecord')
         // This test passes because it does not throw an exception.
-        chai.assert.strictEqual(1, 1);
+        const server = new Server(blueskyClient)
+        // 
+        console.log()
+        const response = await inject(server.app, {
+            method: 'POST',
+            url: '/post-published',
+            payload:JSON.stringify({
+                post:
+                {
+                    current:
+                    {
+                        title: 'test title',
+                        extract: 'test extract',
+                        url: 'https://test.com'
+                    }
+                }
+            })
+        });
+        chai.expect(response.payload).to.equal('Successfully posted to bluesky!');
+        chai.expect(response.statusCode).to.equal(200);
+        chai.assert.strictEqual(blueskyClient.createBlueskySession.mock.callCount(), 1);
+        chai.assert.strictEqual(blueskyClient.createRecord.mock.callCount(), 1);
     });
 }
